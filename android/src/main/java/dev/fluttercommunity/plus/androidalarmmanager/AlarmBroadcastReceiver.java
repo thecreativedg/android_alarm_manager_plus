86e6ecdf20b9f2afa751ad68b45e0aef11f2bd48
Davor G <thecreativedg@gmail.com>
2023-03-28 07:02:04 +0200

    wake up fix

--- android/src/main/java/dev/fluttercommunity/plus/androidalarmmanager/AlarmBroadcastReceiver.java
+++ android/src/main/java/dev/fluttercommunity/plus/androidalarmmanager/AlarmBroadcastReceiver.java
@@ -8,24 +8,34 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import android.os.PowerManager;
+import android.content.pm.PackageManager;
+
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
-  /**
-   * Invoked by the OS when a timer goes off.
-   *
-   * <p>The associated timer was registered in {@link AlarmService}.
-   *
-   * <p>In Android, timer notifications require a {@link BroadcastReceiver} as the artifact that is
-   * notified when the timer goes off. As a result, this method is kept simple, immediately
-   * offloading any work to {@link AlarmService#enqueueAlarmProcessing(Context, Intent)}.
-   *
-   * <p>This method is the beginning of an execution path that will eventually execute a desired
-   * Dart callback function, as registered by the Dart side of the android_alarm_manager plugin.
-   * However, there may be asynchronous gaps between {@code onReceive()} and the eventual invocation
-   * of the Dart callback because {@link AlarmService} may need to spin up a Flutter execution
-   * context before the callback can be invoked.
-   */
-  @Override
+        
+  private static PowerManager.WakeLock wakeLock;
+
+  @OverRide
   public void onReceive(Context context, Intent intent) {
+    PowerManager powerManager = (PowerManager)
+context.getSystemService(Context.POWER_SERVICE);
+    wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK |
+            PowerManager.ACQUIRE_CAUSES_WAKEUP |
+            PowerManager.ON_AFTER_RELEASE, "My wakelock");
+
+    Intent startIntent = context
+            .getPackageManager()
+            .getLaunchIntentForPackage(context.getPackageName());
+
+    startIntent.setFlags(
+            Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
+                    Intent.FLAG_ACTIVITY_NEW_TASK |
+                    Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+    );
+
+    wakeLock.acquire();
+    context.startActivity(startIntent);
     AlarmService.enqueueAlarmProcessing(context, intent);
+    wakeLock.release();
   }
 }
